9059e64868d8b4acb09ff22485e66c83b76436c5
Brent Farris <brentfarris@outlook.com>
2017-02-06 22:31:26 -0800

    Updating the multiplayer menu to use nat easily

--- Forge Networking Remastered Unity/Assets/Bearded Man Studios Inc/Scripts/Multiplayer Menu/MultiplayerMenu.cs
+++ Forge Networking Remastered Unity/Assets/Bearded Man Studios Inc/Scripts/Multiplayer Menu/MultiplayerMenu.cs
@@ -1,6 +1,5 @@
 ﻿using BeardedManStudios.Forge.Networking;
 using BeardedManStudios.Forge.Networking.Unity;
-using BeardedManStudios.Forge.Networking.Lobby;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -8,184 +7,194 @@ using UnityEngine.UI;
 
 public class MultiplayerMenu : MonoBehaviour
 {
-	public InputField ipAddress = null;
-	public InputField portNumber = null;
-	public bool DontChangeSceneOnConnect = false;
-	public string masterServerHost = string.Empty;
-	public ushort masterServerPort = 15940;
-	public bool connectUsingMatchmaking = false;
-	public bool useElo = false;
-	public int myElo = 0;
-	public int eloRequired = 0;
+    public InputField ipAddress = null;
+    public InputField portNumber = null;
+    public bool DontChangeSceneOnConnect = false;
+    public string masterServerHost = string.Empty;
+    public ushort masterServerPort = 15940;
+    public string natServerHost = string.Empty;
+    public ushort natServerPort = 15940;
+    public bool connectUsingMatchmaking = false;
+    public bool useElo = false;
+    public int myElo = 0;
+    public int eloRequired = 0;
 
-	public GameObject networkManager = null;
-	public GameObject[] ToggledButtons;
-	private NetworkManager mgr = null;
+    public GameObject networkManager = null;
+    public GameObject[] ToggledButtons;
+    private NetworkManager mgr = null;
 
-	private List<Button> _uiButtons = new List<Button>();
-	private bool _matchmaking = false;
-	public bool useMainThreadManagerForRPCs = true;
-	public bool useInlineChat = false;
+    private List<Button> _uiButtons = new List<Button>();
+    private bool _matchmaking = false;
+    public bool useMainThreadManagerForRPCs = true;
+    public bool useInlineChat = false;
 
-	public bool getLocalNetworkConnections = false;
+    public bool getLocalNetworkConnections = false;
 
-	public bool useTCP = false;
+    public bool useTCP = false;
 
-	private void Start()
-	{
-		ipAddress.text = "127.0.0.1";
-		portNumber.text = "15937";
+    private void Start()
+    {
+        ipAddress.text = "127.0.0.1";
+        portNumber.text = "15937";
 
-		for (int i = 0; i < ToggledButtons.Length; ++i)
-		{
-			Button btn = ToggledButtons[i].GetComponent<Button>();
-			if (btn != null)
-				_uiButtons.Add(btn);
-		}
+        for (int i = 0; i < ToggledButtons.Length; ++i)
+        {
+            Button btn = ToggledButtons[i].GetComponent<Button>();
+            if (btn != null)
+                _uiButtons.Add(btn);
+        }
 
-		if (!useTCP)
-		{
-			// Do any firewall opening requests on the operating system
-			NetWorker.PingForFirewall();
-		}
+        if (!useTCP)
+        {
+            // Do any firewall opening requests on the operating system
+            NetWorker.PingForFirewall();
+        }
 
-		if (useMainThreadManagerForRPCs)
-			Rpc.MainThreadRunner = MainThreadManager.Instance;
+        if (useMainThreadManagerForRPCs)
+            Rpc.MainThreadRunner = MainThreadManager.Instance;
 
-		if (getLocalNetworkConnections)
-			NetWorker.SetupLocalUdpListings();
-	}
+        if (getLocalNetworkConnections)
+            NetWorker.SetupLocalUdpListings();
+    }
 
-	public void Connect()
-	{
-		if (connectUsingMatchmaking)
-		{
-			ConnectToMatchmaking();
-			return;
-		}
-		int port = ushort.Parse(portNumber.text);
-		if (port < 0 || port > ushort.MaxValue)
-		{
-			Debug.LogError("The supplied port number is not within the allowed range 0-" + ushort.MaxValue);
-			return;
-		}
+    public void Connect()
+    {
+        if (connectUsingMatchmaking)
+        {
+            ConnectToMatchmaking();
+            return;
+        }
+        int port = ushort.Parse(portNumber.text);
+        if (port < 0 || port > ushort.MaxValue)
+        {
+            Debug.LogError("The supplied port number is not within the allowed range 0-" + ushort.MaxValue);
+            return;
+        }
 
-		NetWorker client;
+        NetWorker client;
 
-		if (useTCP)
-		{
-			client = new TCPClient();
-			((TCPClient)client).Connect(ipAddress.text, (ushort)port);
-		}
-		else
-		{
-			client = new UDPClient();
-			((UDPClient)client).Connect(ipAddress.text, (ushort)port);
-		}
+        if (useTCP)
+        {
+            client = new TCPClient();
+            ((TCPClient)client).Connect(ipAddress.text, (ushort)port);
+        }
+        else
+        {
+            client = new UDPClient();
 
-		Connected(client);
-	}
+            if (natServerHost.Trim().Length == 0)
+                ((UDPClient)client).Connect(ipAddress.text, (ushort)port);
+            else
+                ((UDPClient)client).Connect(ipAddress.text, (ushort)port, natServerHost, natServerPort);
+        }
 
-	public void ConnectToMatchmaking()
-	{
-		if (_matchmaking)
-			return;
+        Connected(client);
+    }
 
-		SetToggledButtons(false);
-		_matchmaking = true;
+    public void ConnectToMatchmaking()
+    {
+        if (_matchmaking)
+            return;
 
-		if (mgr == null && networkManager == null)
-		{
-			Debug.LogWarning("A network manager was not provided, generating a new one instead");
-			networkManager = new GameObject("Network Manager");
-			mgr = networkManager.AddComponent<NetworkManager>();
-		}
-		else if (mgr == null)
-			mgr = Instantiate(networkManager).GetComponent<NetworkManager>();
+        SetToggledButtons(false);
+        _matchmaking = true;
 
-		mgr.MatchmakingServersFromMasterServer(masterServerHost, masterServerPort, myElo, (response) =>
-		{
-			_matchmaking = false;
-			SetToggledButtons(true);
-			Debug.LogFormat("Matching Server(s) count[{0}]", response.serverResponse.Count);
+        if (mgr == null && networkManager == null)
+        {
+            Debug.LogWarning("A network manager was not provided, generating a new one instead");
+            networkManager = new GameObject("Network Manager");
+            mgr = networkManager.AddComponent<NetworkManager>();
+        }
+        else if (mgr == null)
+            mgr = Instantiate(networkManager).GetComponent<NetworkManager>();
 
-			//TODO: YOUR OWN MATCHMAKING EXTRA LOGIC HERE!
-			// I just make it randomly pick a server... you can do whatever you please!
-			if (response != null && response.serverResponse.Count > 0)
-			{
-				MasterServerResponse.Server server = response.serverResponse[Random.Range(0, response.serverResponse.Count)];
-				//TCPClient client = new TCPClient();
-				UDPClient client = new UDPClient();
-				client.Connect(server.Address, server.Port);
-				Connected(client);
-			}
-		});
-	}
+        mgr.MatchmakingServersFromMasterServer(masterServerHost, masterServerPort, myElo, (response) =>
+        {
+            _matchmaking = false;
+            SetToggledButtons(true);
+            Debug.LogFormat("Matching Server(s) count[{0}]", response.serverResponse.Count);
 
-	public void Host()
-	{
-		NetWorker server;
+            //TODO: YOUR OWN MATCHMAKING EXTRA LOGIC HERE!
+            // I just make it randomly pick a server... you can do whatever you please!
+            if (response != null && response.serverResponse.Count > 0)
+            {
+                MasterServerResponse.Server server = response.serverResponse[Random.Range(0, response.serverResponse.Count)];
+                //TCPClient client = new TCPClient();
+                UDPClient client = new UDPClient();
+                client.Connect(server.Address, server.Port);
+                Connected(client);
+            }
+        });
+    }
 
-		if (useTCP)
-		{
-			server = new TCPServer(64);
-			((TCPServer)server).Connect();
-		}
-		else
-		{
-			server = new UDPServer(64);
-			((UDPServer)server).Connect();
-		}
-		//TODO: Implement Lobby Service
-		//LobbyService.Instance.Initialize(server);
+    public void Host()
+    {
+        NetWorker server;
 
-		Connected(server);
-	}
+        if (useTCP)
+        {
+            server = new TCPServer(64);
+            ((TCPServer)server).Connect();
+        }
+        else
+        {
+            server = new UDPServer(64);
 
-	public void Connected(NetWorker networker)
-	{
-		if (!networker.IsBound)
-		{
-			Debug.LogError("NetWorker failed to bind");
-			return;
-		}
+            if (natServerHost.Trim().Length == 0)
+                ((UDPServer)server).Connect();
+            else
+                ((UDPServer)server).Connect(natHost: natServerHost, natPort: natServerPort);
+        }
+        //TODO: Implement Lobby Service
+        //LobbyService.Instance.Initialize(server);
 
-		if (mgr == null && networkManager == null)
-		{
-			Debug.LogWarning("A network manager was not provided, generating a new one instead");
-			networkManager = new GameObject("Network Manager");
-			mgr = networkManager.AddComponent<NetworkManager>();
-		}
-		else if (mgr == null)
-			mgr = Instantiate(networkManager).GetComponent<NetworkManager>();
+        Connected(server);
+    }
 
-		mgr.Initialize(networker, masterServerHost, masterServerPort, useElo, eloRequired);
-		
-		if (useInlineChat && networker.IsServer)
-			SceneManager.sceneLoaded += CreateInlineChat;
+    public void Connected(NetWorker networker)
+    {
+        if (!networker.IsBound)
+        {
+            Debug.LogError("NetWorker failed to bind");
+            return;
+        }
 
-		if (!DontChangeSceneOnConnect)
-			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-		else
-			NetworkObject.Flush(networker); //Called because we are already in the correct scene!
-	}
+        if (mgr == null && networkManager == null)
+        {
+            Debug.LogWarning("A network manager was not provided, generating a new one instead");
+            networkManager = new GameObject("Network Manager");
+            mgr = networkManager.AddComponent<NetworkManager>();
+        }
+        else if (mgr == null)
+            mgr = Instantiate(networkManager).GetComponent<NetworkManager>();
 
-	private void CreateInlineChat(Scene arg0, LoadSceneMode arg1)
-	{
-		SceneManager.sceneLoaded -= CreateInlineChat;
-		var chat = NetworkManager.Instance.InstantiateChatManagerNetworkObject();
-		DontDestroyOnLoad(chat.gameObject);
-	}
+        mgr.Initialize(networker, masterServerHost, masterServerPort, useElo, eloRequired);
 
-	private void SetToggledButtons(bool value)
-	{
-		for (int i = 0; i < _uiButtons.Count; ++i)
-			_uiButtons[i].interactable = value;
-	}
+        if (useInlineChat && networker.IsServer)
+            SceneManager.sceneLoaded += CreateInlineChat;
 
-	private void OnApplicationQuit()
-	{
-		if (getLocalNetworkConnections)
-			NetWorker.ApplicationExit();
-	}
+        if (!DontChangeSceneOnConnect)
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        else
+            NetworkObject.Flush(networker); //Called because we are already in the correct scene!
+    }
+
+    private void CreateInlineChat(Scene arg0, LoadSceneMode arg1)
+    {
+        SceneManager.sceneLoaded -= CreateInlineChat;
+        var chat = NetworkManager.Instance.InstantiateChatManagerNetworkObject();
+        DontDestroyOnLoad(chat.gameObject);
+    }
+
+    private void SetToggledButtons(bool value)
+    {
+        for (int i = 0; i < _uiButtons.Count; ++i)
+            _uiButtons[i].interactable = value;
+    }
+
+    private void OnApplicationQuit()
+    {
+        if (getLocalNetworkConnections)
+            NetWorker.ApplicationExit();
+    }
 }
