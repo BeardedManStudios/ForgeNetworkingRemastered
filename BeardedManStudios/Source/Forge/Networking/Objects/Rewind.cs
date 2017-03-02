d3e7bdfebc4cb58467cbc0867b107d99b560c2bd
Brent Farris <brentfarris@outlook.com>
2017-03-02 13:25:46 -0800

    Fixing bugs and optimizing Rewind object

--- BeardedManStudios/Source/Forge/Networking/Objects/Rewind.cs
+++ BeardedManStudios/Source/Forge/Networking/Objects/Rewind.cs
@@ -22,148 +22,209 @@ using System.Linq;
 
 namespace BeardedManStudios.Forge.Networking
 {
-	public class Rewind<T>
-	{
-		public TimeManager Time { get; set; }
-		public ulong rewindTime = 0;
-		private Dictionary<ulong, T> rewinds = null;
+    public class Rewind<T>
+    {
+        public ulong RewindTime { get; set; }
+        private Dictionary<ulong, T> rewinds = null;
+        private List<ulong> keys = new List<ulong>();
 
-		public void Register(T value, ulong timestep = 0)
-		{
-			if (timestep == 0)
-				timestep = Time.Timestep;
+        public Rewind(ulong length)
+        {
+            RewindTime = length;
+            rewinds = new Dictionary<ulong, T>();
+        }
 
-			if (rewinds.Count > 0)
-			{
-				var keys = rewinds.Keys;
-				foreach (ulong key in keys)
-				{
-					if (key < timestep - rewindTime)
-						rewinds.Remove(key);
-				}
-			}
+        public void Register(T value, ulong timestep = 0)
+        {
+            lock (rewinds)
+            {
+                if (keys.Contains(timestep))
+                {
+                    rewinds[timestep] = value;
+                    return;
+                }
 
-			rewinds.Add(timestep, value);
-		}
+                if (rewinds.Count > 0)
+                {
+                    for (int i = 0; i < keys.Count; i++)
+                    {
+                        if (keys[i] < timestep - RewindTime && RewindTime < timestep)
+                        {
+                            rewinds.Remove(keys[i]);
+                            keys.RemoveAt(i--);
+                        }
+                        else
+                            break;
+                    }
+                }
 
-		/// <summary>
-		/// Used to get the value of a variable based on a given time step
-		/// </summary>
-		/// <param name="timestep">The time step that a value should be pulled for</param>
-		/// <returns>The value of the tracked variable at the given time stamp or default() if no history is available</returns>
-		public T Get(ulong timestep)
-		{
-			if (rewinds.Count == 0)
-				return default(T);
+                rewinds.Add(timestep, value);
+                keys.Add(timestep);
+            }
+        }
 
-			if (rewinds.ContainsKey(timestep))
-				return rewinds[timestep];
+        /// <summary>
+        /// Used to get the value of a variable based on a given time step
+        /// </summary>
+        /// <param name="timestep">The time step that a value should be pulled for</param>
+        /// <returns>The value of the tracked variable at the given time stamp or default() if no history is available</returns>
+        public T Get(ulong timestep)
+        {
+            lock (rewinds)
+            {
+                if (rewinds.Count == 0)
+                    return default(T);
 
-			var keys = rewinds.Keys;
-			ulong key = 0;
+                T result;
+                if (rewinds.TryGetValue(timestep, out result))
+                    return result;
 
-			foreach (ulong k in keys)
-			{
-				if (timestep < k)
-				{
-					if (timestep - key > k - timestep) key = k;
-					break;
-				}
+                ulong key = 0;
+                foreach (ulong k in keys)
+                {
+                    if (timestep < k)
+                    {
+                        if (timestep - key > k - timestep)
+                            key = k;
 
-				key = k;
-			}
+                        break;
+                    }
 
-			return rewinds[key];
-		}
+                    key = k;
+                }
 
-		/// <summary>
-		/// Used to get the value of a variable based on a given time step but also
-		/// will out the lower and upper if found to allow for getting median
-		/// </summary>
-		/// <param name="timestep">The time step that a value should be pulled for</param>
-		/// <param name="lower">The lover value relative to the timestep</param>
-		/// <param name="upper">The upper value relative to the timestep</param>
-		/// <returns>The value of the tracked variable at the given time stamp or default() if no history is available</returns>
-		public T Get(ulong timestep, out T lower, out T upper)
-		{
-			lower = default(T);
-			upper = lower;
+                return rewinds[key];
+            }
+        }
 
-			if (rewinds.Count == 0)
-				return lower;
+        /// <summary>
+        /// Used to get the value of a variable based on a given time step but also
+        /// will out the lower and upper if found to allow for getting median
+        /// </summary>
+        /// <param name="timestep">The time step that a value should be pulled for</param>
+        /// <param name="lower">The lover value relative to the timestep</param>
+        /// <param name="upper">The upper value relative to the timestep</param>
+        /// <returns>The value of the tracked variable at the given time stamp or default() if no history is available</returns>
+        public T Get(ulong timestep, out T lower, out T upper)
+        {
+            lock (rewinds)
+            {
+                lower = default(T);
+                upper = lower;
 
-			if (rewinds.ContainsKey(timestep))
-				return rewinds[timestep];
+                if (rewinds.Count == 0)
+                    return lower;
 
-			var keys = rewinds.Keys;
-			ulong key = 0, lowerKey = 0, upperKey = 0;
+                if (rewinds.ContainsKey(timestep))
+                    return rewinds[timestep];
 
-			if (keys.Last() < timestep)
-			{
-				// TODO:  The supplied time does not exist, throw exception?
-				return default(T);
-			}
+                ulong key = 0, lowerKey = 0, upperKey = 0;
 
-			foreach (ulong k in keys)
-			{
-				if (timestep < k)
-				{
-					lowerKey = key;
-					upperKey = k;
-					if (timestep - key > k - timestep) key = k;
-					break;
-				}
+                if (keys.Last() < timestep)
+                {
+                    // TODO:  The supplied time does not exist, throw exception?
+                    return default(T);
+                }
 
-				key = k;
-			}
+                foreach (ulong k in keys)
+                {
+                    if (timestep < k)
+                    {
+                        lowerKey = key;
+                        upperKey = k;
+                        if (timestep - key > k - timestep)
+                            key = k;
 
-			lower = rewinds[lowerKey];
-			upper = rewinds[upperKey];
-			return rewinds[key];
-		}
+                        break;
+                    }
 
-		/// <summary>
-		/// Gets a range of values leading up to a given time step
-		/// </summary>
-		/// <param name="timestep">The time step that a value should be pulled for</param>
-		/// <param name="count">The number of elements to get before the time step</param>
-		/// <returns>The list of found elements leading up to the timestep (including time step value if found</returns>
-		public List<T> GetRange(ulong timestep, int count)
-		{
-			List<T> found = new List<T>();
+                    key = k;
+                }
 
-			if (rewinds.Count == 0)
-				return found;
+                lower = rewinds[lowerKey];
+                upper = rewinds[upperKey];
+                return rewinds[key];
+            }
+        }
 
-			var keys = rewinds.Keys.ToArray().Reverse();
-			ulong lastKey = 0;
+        /// <summary>
+        /// Gets a range of values leading up to a given time step
+        /// </summary>
+        /// <param name="timestep">The time step that a value should be pulled for</param>
+        /// <param name="count">The number of elements to get before the time step</param>
+        /// <returns>The list of found elements leading up to the timestep (including time step value if found</returns>
+        public List<T> GetRange(ulong timestep, int count)
+        {
+            lock (rewinds)
+            {
+                List<T> found = new List<T>();
 
-			if (keys.First() < timestep)
-			{
-				// TODO:  The supplied time does not exist, throw exception?
-				return found;
-			}
+                if (rewinds.Count == 0)
+                    return found;
 
-			foreach (ulong k in keys)
-			{
-				if (timestep <= k)
-				{
-					if (timestep != k && lastKey != 0)
-					{
-						found.Add(rewinds[lastKey]);
-						lastKey = 0;
-					}
+                var keys = rewinds.Keys.ToArray().Reverse();
+                ulong lastKey = 0;
 
-					found.Insert(0, rewinds[k]);
+                if (keys.First() < timestep)
+                {
+                    // TODO:  The supplied time does not exist, throw exception?
+                    return found;
+                }
 
-					if (found.Count == count)
-						break;
-				}
-				else
-					lastKey = k;
-			}
+                foreach (ulong k in keys)
+                {
+                    if (timestep <= k)
+                    {
+                        if (timestep != k && lastKey != 0)
+                        {
+                            found.Add(rewinds[lastKey]);
+                            lastKey = 0;
+                        }
 
-			return found;
-		}
-	}
+                        found.Insert(0, rewinds[k]);
+
+                        if (found.Count == count)
+                            break;
+                    }
+                    else
+                        lastKey = k;
+                }
+
+                return found;
+            }
+        }
+
+        /// <summary>
+        /// Gets a range of values between the two timesteps (inclusive)
+        /// </summary>
+        /// <param name="timestepMin">The minimum inclusive timestep that a value should be pulled for</param>
+        /// <param name="timestepMax">The maximum inclusive timestep that a value should be pulled for</param>
+        /// <returns>The list of found elements between the two timesteps inclusively </returns>
+        public List<T> GetRange(ulong timestepMin, ulong timestepMax)
+        {
+            lock (rewinds)
+            {
+                List<T> found = new List<T>();
+
+                if (rewinds.Count == 0)
+                    return found;
+
+                var keys = rewinds.Keys.ToArray().Reverse();
+
+                if (keys.First() < timestepMin)
+                {
+                    // TODO:  The supplied time does not exist, throw exception?
+                    return found;
+                }
+
+                foreach (ulong k in keys)
+                {
+                    if (timestepMin <= k && timestepMax >= k)
+                        found.Insert(0, rewinds[k]);
+                }
+
+                return found;
+            }
+        }
+    }
 }
