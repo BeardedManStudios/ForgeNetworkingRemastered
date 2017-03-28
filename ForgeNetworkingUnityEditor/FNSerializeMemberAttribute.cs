0c8e45dd7e89b383919506532589558dd2df5843
Crazy8ball <brfaulds@gmail.com>
2017-03-27 23:56:02 -0700

    JSON Serialization WIP

---
+++ ForgeNetworkingUnityEditor/FNSerializeMemberAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BeardedManStudios.Forge.Networking.UnityEditor.Serializer
+{
+	/// <summary>
+	/// Declare a member as serializable
+	/// </summary>
+	[AttributeUsage(AttributeTargets.Field)]
+	public class FNSerializeMemberAttribute : Attribute
+	{
+	}
+}
