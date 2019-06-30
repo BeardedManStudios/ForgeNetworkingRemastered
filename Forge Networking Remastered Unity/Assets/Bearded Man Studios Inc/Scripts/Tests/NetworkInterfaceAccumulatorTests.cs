5b3675386a5d1c55733d7b6053b6f2b57a5731c4
Brent Farris <brentfarris@outlook.com>
2019-06-30 02:14:23 -0700

    Trying out the unit test stuff in Unity

---
+++ Forge Networking Remastered Unity/Assets/Bearded Man Studios Inc/Scripts/Tests/NetworkInterfaceAccumulatorTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+
+namespace BeardedManStudios.Forge.Networking.Tests
+{
+	public class NetworkInterfaceAccumulatorTests
+	{
+		[Test]
+		public void CheckToMakeSureSomethingExistsOnTheNetworkIPv4()
+		{
+			// Our computer should be listed on the network
+			var a = new NetworkInterfaceAccumulator();
+			a.Accumulate(System.Net.Sockets.AddressFamily.InterNetwork);
+			Assert.AreNotEqual(0, a.IPs.Count);
+		}
+
+		[Test]
+		public void CheckToMakeSureSomethingExistsOnTheNetworkIPv6()
+		{
+			// Our computer should be listed on the network
+			var a = new NetworkInterfaceAccumulator();
+			a.Accumulate(System.Net.Sockets.AddressFamily.InterNetworkV6);
+			Assert.AreNotEqual(0, a.IPs.Count);
+		}
+	}
+}
