c48a9113e6103ccb1b2509756a85855aeb853eea
DrPotato <44233230+DrPotatoNet@users.noreply.github.com>
2019-08-12 18:13:55 -0500

    Added StandAloneNetworkobjectFactoryTemplate and Added to the Float2-4 for the Code Generation

--- ForgeUnity/Assets/BeardedManStudios/Scripts/StandAlone/Objects/Float3.cs
+++ ForgeUnity/Assets/BeardedManStudios/Scripts/StandAlone/Objects/Float3.cs
@@ -10,5 +10,29 @@
 			this.y = y;
 			this.z = z;
 		}
+
+		public static bool operator ==(Float3 a, Float3 b)
+		{
+			return a.x == b.x && a.y == b.y && a.z == b.z;
+		}
+
+		public static bool operator !=(Float3 a, Float3 b)
+		{
+			return !(a == b);
+		}
+
+		public override bool Equals(object obj)
+		{
+			if (!(obj is Float3))
+				return false;
+
+			Float3 other = (Float3)obj;
+			return x == other.x && y == other.y && z == other.z;
+		}
+
+		public override int GetHashCode()
+		{
+			return base.GetHashCode();
+		}
 	}
 }
--- ForgeUnity/Assets/BeardedManStudios/Scripts/StandAlone/Objects/Float4.cs
+++ ForgeUnity/Assets/BeardedManStudios/Scripts/StandAlone/Objects/Float4.cs
@@ -11,5 +11,29 @@
 			this.z = z;
 			this.w = w;
 		}
+
+		public static bool operator ==(Float4 a, Float4 b)
+		{
+			return a.x == b.x && a.y == b.y && a.z == b.z && a.w == b.w;
+		}
+
+		public static bool operator !=(Float4 a, Float4 b)
+		{
+			return !(a == b);
+		}
+
+		public override bool Equals(object obj)
+		{
+			if (!(obj is Float4))
+				return false;
+
+			Float4 other = (Float4)obj;
+			return x == other.x && y == other.y && z == other.z && w == other.w;
+		}
+
+		public override int GetHashCode()
+		{
+			return base.GetHashCode();
+		}
 	}
 }
