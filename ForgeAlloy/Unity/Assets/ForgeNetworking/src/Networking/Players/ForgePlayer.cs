56fe0dff33ae1f929404562a60b15a3958ac1a1f
Brent Farris <brentfarris@outlook.com>
2019-11-08 13:09:01 -0800

    Moving the folders around

---
+++ ForgeAlloy/Unity/Assets/ForgeNetworking/src/Networking/Players/ForgePlayer.cs
@@ -0,0 +1,10 @@
+﻿namespace Forge.Networking.Players
+{
+	public class ForgePlayer : INetPlayer
+	{
+		public int Id { get; private set; }
+		public string Ip { get; private set; }
+		public ushort Port { get; private set; }
+		public string Name { get; private set; }
+	}
+}
