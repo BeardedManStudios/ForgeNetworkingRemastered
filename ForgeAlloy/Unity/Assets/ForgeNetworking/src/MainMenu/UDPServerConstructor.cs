167c0c4ac402054ecf52fd407a17be285c080fe5
Crazy8ball <brfaulds@gmail.com>
2019-11-14 00:16:54 -0800

    UI WIP

--- ForgeAlloy/Unity/Assets/ForgeNetworking/src/MainMenu/UDPServerConstructor.cs
+++ ForgeAlloy/Unity/Assets/ForgeNetworking/src/MainMenu/UDPServerConstructor.cs
@@ -2,20 +2,29 @@
 using Forge.Factory;
 using Forge.Networking.Sockets;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Forge.Networking.Unity
 {
 	public class UDPServerConstructor : MonoBehaviour, IUDPServerConstructor
 	{
 		[SerializeField]
-		private ushort _port;
+		private ushort _port = 15937;
 		public ushort Port => _port;
 
 		[SerializeField]
-		private int _maxPlayers;
+		private int _maxPlayers = 32;
 		public int MaxPlayers => _maxPlayers;
 
+		[SerializeField]
+		private Text _hostLabel;
+
 		private ISocketServerFacade _socketFacade;
+
+		private void Awake()
+		{
+			_hostLabel.text = $"(h) Host (127.0.0.1:{ _port })";
+		}
 
 		public INetworkMediator CreateAndStartServer(IEngineContainer engineContainer)
 		{
---
+++ ForgeAlloy/Unity/Assets/ForgeNetworking/src/UI/UIButton.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+namespace Forge.Networking.Unity.UI
+{
+	public class UIButton : MonoBehaviour, IUIButton
+	{
+		private Action _callback;
+
+		public void RegisterCallback(Action callback)
+		{
+			_callback = callback;
+		}
+
+		public void Raise()
+		{
+			if (_callback != null)
+			{
+				_callback();
+			}
+		}
+	}
+}
