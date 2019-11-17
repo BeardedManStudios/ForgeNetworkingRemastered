d2527a656ce47b00b7615c382e6a910eaeb6d415
Brent Farris <brentfarris@outlook.com>
2019-11-16 18:51:58 -0800

    Moving the repository namespace

--- ForgeAlloy/Forge/Repository/KeyedRepository.cs
+++ ForgeAlloy/Forge/Repository/KeyedRepository.cs
@@ -1,35 +1,35 @@
 ﻿using System.Collections.Generic;
 
-namespace Forge.DataStructures.Repository
+namespace Forge.Repository
 {
 	public class KeyedRepository<TKey, TObject> : IRepository<TKey, TObject>
 	{
 		public event RepositoryChanged<TObject> onAdd;
 		public event RepositoryChanged<TObject> onRemove;
 
-		private readonly Dictionary<TKey, TObject> _dict = new Dictionary<TKey, TObject>();
+		protected readonly Dictionary<TKey, TObject> dict = new Dictionary<TKey, TObject>();
 
-		public void Add(TKey key, TObject obj)
+		public virtual void Add(TKey key, TObject obj)
 		{
-			if (_dict.ContainsKey(key))
+			if (dict.ContainsKey(key))
 			{
 				throw new KeyNotFoundException($"The key {key} could not" +
 					$" be located in the repository");
 			}
-			_dict.Add(key, obj);
+			dict.Add(key, obj);
 			onAdd?.Invoke(obj);
 		}
 
-		public bool Exists(TKey key)
+		public virtual bool Exists(TKey key)
 		{
-			return _dict.ContainsKey(key);
+			return dict.ContainsKey(key);
 		}
 
-		public void Remove(TKey key)
+		public virtual void Remove(TKey key)
 		{
-			if (_dict.TryGetValue(key, out var obj))
+			if (dict.TryGetValue(key, out var obj))
 			{
-				_dict.Remove(key);
+				dict.Remove(key);
 				onRemove?.Invoke(obj);
 			}
 		}
