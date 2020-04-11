c3438fd3eda30ff1dace08dbb1cd3ac00cbbd827
Brent Farris <brentfarris@outlook.com>
2020-04-10 17:14:52 -0700

    Fixing pooling performance
    List.Remove(0) has the worst remove performance from a list since it removes back to front (see MS documentation). Utilizing a queue instead for performance and clarity. Also there is no need to keep track of the messages that are in use, they will return when they return.

--- ForgeAlloy/ForgeAlloyUnity/Assets/ForgeNetworking/Networking/Messaging/MessagePool.cs
+++ ForgeAlloy/ForgeAlloyUnity/Assets/ForgeNetworking/Networking/Messaging/MessagePool.cs
@@ -1,11 +1,10 @@
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace Forge.Networking.Messaging
 {
 	public class MessagePool<T> where T : IMessage, new()
 	{
-		private readonly List<T> _poolAvailable = new List<T>();
-		private readonly List<T> _poolInUse = new List<T>();
+		private readonly Queue<T> _poolAvailable = new Queue<T>();
 
 		public T Get()
 		{
@@ -19,23 +18,19 @@ namespace Forge.Networking.Messaging
 		{
 			var m = new T();
 			m.OnMessageSent += Release;
-			_poolInUse.Add(m);
 			return m;
 		}
 
 		private T GetAvailable()
 		{
-			var m = _poolAvailable[0];
-			_poolAvailable.RemoveAt(0);
-			_poolInUse.Add(m);
+			var m = _poolAvailable.Dequeue();
 			return m;
 		}
 
 		private void Release(IMessage message)
 		{
 			var m = (T)message;
-			_poolInUse.Remove(m);
-			_poolAvailable.Add(m);
+			_poolAvailable.Enqueue(m);
 		}
 	}
 }
--- ForgeAlloy/ForgeAlloyUnity/Assets/ForgeNetworking/Networking/Messaging/MessagePoolMulti.cs
+++ ForgeAlloy/ForgeAlloyUnity/Assets/ForgeNetworking/Networking/Messaging/MessagePoolMulti.cs
@@ -1,75 +1,58 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 
 namespace Forge.Networking.Messaging
 {
 	public class MessagePoolMulti
 	{
-		private class Pool
-		{
-			public List<IMessage> Available = new List<IMessage>();
-			public List<IMessage> InUse = new List<IMessage>();
-		}
-
-		private readonly Dictionary<Type, Pool> _messagePools = new Dictionary<Type, Pool>();
+		private readonly Dictionary<Type, Queue<IMessage>> _messagePools = new Dictionary<Type, Queue<IMessage>>();
 
 		public IMessage Get(Type t)
 		{
 			var pool = GetPool(t);
-			if (pool.Available.Count == 0)
+			if (pool.Count == 0)
 				return CreateNewMessageForPool(t, pool);
 			else
-				return GetAvailable(pool);
+				return pool.Dequeue();
 		}
 
 		public T Get<T>() where T : IMessage, new()
 		{
 			var pool = GetPool(typeof(T));
-			if (pool.Available.Count == 0)
+			if (pool.Count == 0)
 				return CreateNewMessageForPool<T>(pool);
 			else
-				return (T)GetAvailable(pool);
+				return (T)pool.Dequeue();
 		}
 
-		private Pool GetPool(Type type)
+		private Queue<IMessage> GetPool(Type type)
 		{
 			if (!_messagePools.TryGetValue(type, out var pool))
 			{
-				pool = new Pool();
+				pool = new Queue<IMessage>();
 				_messagePools.Add(type, pool);
 			}
 			return pool;
 		}
 
-		private T CreateNewMessageForPool<T>(Pool pool) where T : IMessage, new()
+		private T CreateNewMessageForPool<T>(Queue<IMessage> pool) where T : IMessage, new()
 		{
 			T m = new T();
 			m.OnMessageSent += Release;
-			pool.InUse.Add(m);
 			return m;
 		}
 
-		private IMessage CreateNewMessageForPool(Type t, Pool pool)
+		private IMessage CreateNewMessageForPool(Type t, Queue<IMessage> pool)
 		{
 			IMessage m = (IMessage)Activator.CreateInstance(t);
 			m.OnMessageSent += Release;
-			pool.InUse.Add(m);
 			return m;
 		}
 
 		private void Release(IMessage message)
 		{
-			Pool pool = GetPool(message.GetType());
-			pool.InUse.Remove(message);
-			pool.Available.Add(message);
-		}
-
-		private IMessage GetAvailable(Pool pool)
-		{
-			var m = pool.Available[0];
-			pool.Available.RemoveAt(0);
-			pool.InUse.Add(m);
-			return m;
+			Queue<IMessage> pool = GetPool(message.GetType());
+			pool.Enqueue(message);
 		}
 	}
 }
