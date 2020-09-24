ba2f15532a32d70ef29ee82fd59dcd5a15d39c72
Brent Farris <brentfarris@outlook.com>
2020-09-24 12:01:06 -0700

    Adding forward extension and using IsServer property

---
+++ ForgeAlloy/ForgeAlloyUnity/Assets/ForgeNetworking/Networking/MessageExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System.Net;
+using Forge.Networking.Messaging;
+
+namespace Forge.Networking
+{
+	public static class MessageExtensions
+	{
+		public static void ForwardToClients(this INetworkMediator netMediator,
+			EndPoint sender, IMessage message)
+		{
+			if (!netMediator.IsServer)
+			{
+				throw new SocketNotServerException("Tried to forward a message" +
+					"to connected clients, but the current socket is not a server");
+			}
+			var itr = netMediator.PlayerRepository.GetEnumerator();
+			while (itr.MoveNext())
+			{
+				if (itr.Current != null && itr.Current.EndPoint != sender)
+					netMediator.SendReliableMessage(message, itr.Current.EndPoint);
+			}
+		}
+	}
+}
