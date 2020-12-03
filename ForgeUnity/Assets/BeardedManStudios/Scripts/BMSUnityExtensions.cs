acd68b83781e6a3b13e105f57df7c92e0703bf33
Tom Gerrits <tom.gerrits@mrwatts.io>
2020-12-03 10:13:10 +0100

    Replace spaces with tabs as indicated by editorconfig

--- ForgeUnity/Assets/BeardedManStudios/Scripts/BMSUnityExtensions.cs
+++ ForgeUnity/Assets/BeardedManStudios/Scripts/BMSUnityExtensions.cs
@@ -4,24 +4,24 @@ namespace BeardedManStudios.Forge.Networking.Unity
 {
 	public static class BMSUnityExtensions
 	{
-        public static bool Near(this Vector2 target, Vector2 other, float distance = 0.0012f)
+		public static bool Near(this Vector2 target, Vector2 other, float distance = 0.0012f)
 		{
-            return Vector2.Distance(target, other) <= distance;
+			return Vector2.Distance(target, other) <= distance;
 		}
 
-        public static bool Near(this Vector3 target, Vector3 other, float distance = 0.0012f)
+		public static bool Near(this Vector3 target, Vector3 other, float distance = 0.0012f)
 		{
-            return Vector3.Distance(target, other) <= distance;
+			return Vector3.Distance(target, other) <= distance;
 		}
 
-        public static bool Near(this Vector4 target, Vector4 other, float distance = 0.0012f)
+		public static bool Near(this Vector4 target, Vector4 other, float distance = 0.0012f)
 		{
-            return Vector4.Distance(target, other) <= distance;
+			return Vector4.Distance(target, other) <= distance;
 		}
 
-        public static bool Near(this Quaternion target, Quaternion other, float distance = 0.0012f)
+		public static bool Near(this Quaternion target, Quaternion other, float distance = 0.0012f)
 		{
-            return Quaternion.Angle(target, other) <= distance;
-        }
+			return Quaternion.Angle(target, other) <= distance;
+		}
 	}
 }
